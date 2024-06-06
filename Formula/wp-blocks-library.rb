7a51243d985c707ca93eea86a1a314c538b5f719
Luca Pipolo <luca@somoscuatro.es>
2024-06-06 20:56:11 +0200

    chore: update formula sha256

--- Formula/wp-blocks-library.rb
+++ Formula/wp-blocks-library.rb
@@ -2,7 +2,7 @@ class WpBlocksLibrary < Formula
   desc "Scaffolds a WordPress project using Docker and optionally a starter theme"
   homepage "https://github.com/somoscuatro/homebrew-wp-blocks-library"
   url "https://github.com/somoscuatro/homebrew-wp-blocks-library/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 ""
+  sha256 "70b4e04f05a9a65cd9ea124444c152852d1c999b71327e92c9a889ce37ff5ab3"
   license "MIT"
 
   def install
